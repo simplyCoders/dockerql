2691ab6c40c95cac55a066d70cda3fe962e7fc49
ezborgy <ezborgy>
2020-12-24 18:27:50 -0500

    handle pagination for dockerhub

--- src/registries/dockerhub/images.ts
+++ src/registries/dockerhub/images.ts
@@ -2,37 +2,43 @@
 import axios from "axios"
 
 // perform get images
-export const getImages = async (context: any, namespace: string, repo:string): Promise<any[]> => {
+export const getImages = async (context: any, namespace: string, repo: string): Promise<any[]> => {
 
     const endpoint = context.host + "repositories/" + namespace + "/" + repo + "/tags/"
-
+    const records = []
     try {
-        const resp = await axios.get(endpoint,
-            {
-                headers: {
-                    authorization: "Bearer " + context.token
-                }
-            })
+        let nextEndpoint = endpoint
+        while (nextEndpoint !== null) {
+            const resp = await axios.get(nextEndpoint,
+                {
+                    headers: {
+                        authorization: "Bearer " + context.token
+                    }
+                })
 
-        const records = []
-        for (const manifest of resp.data.results) {
-            records.push ({
-                "registry": context.name,
-                "namespace": namespace,
-                "repo": repo,
-                "digest": manifest.images.length===0 ? "" : manifest.images[0].digest,
-                "tag": manifest.name,
-                "size": manifest.full_size,
-                "created": manifest.tag_last_pushed,
-                "pushed": manifest.tag_last_pushed,
-                "pulled": manifest.tag_last_pulled
-            })
+            for (const manifest of resp.data.results) {
+                records.push({
+                    "registry": context.name,
+                    "namespace": namespace,
+                    "repo": repo,
+                    "digest": manifest.images.length === 0 ? "" : manifest.images[0].digest,
+                    "tag": manifest.name,
+                    "size": manifest.full_size,
+                    "created": manifest.tag_last_pushed,
+                    "pushed": manifest.tag_last_pushed,
+                    "pulled": manifest.tag_last_pulled
+                })
+            }
+
+            nextEndpoint = resp.data.next
+            if (nextEndpoint !== null) {
+                console.log("Fetch additional page.")
+            }
         }
-
         console.log("Get images successfull. Count:", records.length)
         return records
     } catch (err) {
-        console.error(JSON.stringify(err).substr(0,800))
+        console.error(JSON.stringify(err).substr(0, 800))
         throw (err)
     }
 }
